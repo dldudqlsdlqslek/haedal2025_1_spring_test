90b06aa938ea28646be34a812527f68fc50e3630
dldsudqlsdlqslek <tina4686@gmail.com>
2025-03-18 18:10:32 +0900

    First commit

---
+++ src/main/java/haedalSpring2025_1/MyFirstProj/MyFirstProjApplication.java
@@ -0,0 +1,13 @@
+package haedalSpring2025_1.MyFirstProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/haedalSpring2025_1/MyFirstProj/MyFirstProjApplicationTests.java
@@ -0,0 +1,13 @@
+package haedalSpring2025_1.MyFirstProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
